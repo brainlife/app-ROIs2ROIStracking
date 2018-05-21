18faa0433c4a54187563ee20968bacd907212862
bacaron <bacaron@imail.iu.edu>
2018-05-21 12:30:27 -0400

    Update trackROI2ROI.sh

--- trackROI2ROI.sh
+++ trackROI2ROI.sh
@@ -170,7 +170,7 @@ for (( i=0; i<=$nTracts; i+=2 ));
 					done
 			done
 		## concatenate tracts
-		holder=(*tract$((i/2+1)).tck)
+		holder=(*tract$((i/2+1))*.tck)
 		cat_tracks track$((i/2+1)).tck ${holder[*]}
 		if [ ! $ret -eq 0 ]; then
 			exit $ret
