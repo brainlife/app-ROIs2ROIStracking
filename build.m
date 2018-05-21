71943a261c358255a7be1f2830528a6bfd951c31
Brad Caron <bacaron@imail.iu.edu>
2018-05-21 18:03:21 -0400

    recompiled

--- build.m
+++ build.m
@@ -2,7 +2,7 @@ addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/encode'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/spm'))
-mcc -m -R -nodisplay -d compiled main
+%mcc -m -R -nodisplay -d compiled main
 mcc -m -R -nodisplay -d classification classificationGenerator
 mcc -m -R -nodisplay -d ROI roiGeneration
 exit
