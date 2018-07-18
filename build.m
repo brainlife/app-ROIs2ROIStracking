bcc5c20c292d154784d94a75bf33db72c9f7ba67
Brad Caron <bacaron@imail.iu.edu>
2018-07-18 09:02:06 -0400

    recompiled classificationGenerator

--- build.m
+++ build.m
@@ -2,6 +2,7 @@ addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/encode'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/spm'))
+addpath(genpath('/N/u/brlife/git/wma'))
 %mcc -m -R -nodisplay -d compiled main
 mcc -m -R -nodisplay -d classification classificationGenerator
 exit
